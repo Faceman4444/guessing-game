d2c5b237fd72bc3f6d4c1c51414860c609dde0f6
Pavel Shinkorenko <pavellubimchik@mail.ru>
2017-10-09 21:15:17 +0300

    Update guessing-game.js
    89/100

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { 
+        this.arr = [];        
+        this.begOfArray = 0;
+        
+    }
 
     setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
+        while(min<=max) {
+            this.arr.push(min);
+            min++;
+        }
+        this.endOfArray = this.arr.length;
+    } 
+    
+    guess() { 
+        this.index = Math.round((this.begOfArray+this.endOfArray)/2);
+        return this.arr[this.index];
+    }   
 
     lower() {
-
+         this.endOfArray = this.index;
     }
 
     greater() {
-
+         this.begOfArray = this.index;
     }
 }
 
